697d1b69b8322d39d436853ba4628c5b81b18e32
fakkui <duque2607f@gmail.com>
2023-05-09 14:27:11 -0500

    Entrega 1.

---
+++ cuadrado.rb
@@ -0,0 +1,5 @@
+puts "Calculate area of a square"
+puts "Enter the value of the sides of the square: "
+L =  gets.chomp.to_i
+resultado = L*L
+print "The area of the square of sides: " +L.to_s+ " is: #{resultado}"
