3ddafd018d2c9ef964e18b7a3f1834733148a703
JuaanDuque <132398922+JuaanDuque@users.noreply.github.com>
2023-07-17 09:41:53 -0500

    Delete circulo.rb

--- circulo.rb
+++
@@ -1,5 +0,0 @@
-puts "Calculate area of a circle"
-puts "Enter the radius of the circle: "
-r = gets.chomp.to_i
-areaCirculo = 3.14 * (r*r)
-print "The area of the circle with radius " +r.to_s + " is #{areaCirculo}"
