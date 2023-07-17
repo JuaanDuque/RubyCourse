9c9f806d861fa284efc034fad681f29f08bf3f5f
JuaanDuque <132398922+JuaanDuque@users.noreply.github.com>
2023-07-17 09:43:01 -0500

    Delete trapecio.rb

--- trapecio.rb
+++
@@ -1,9 +0,0 @@
-puts "Calculate area of a trapezium"
-print "Enter the upper base of the trapezium: "
-bu = gets.chomp.to_i
-print "Enter the lower base of the trapezium: "
-bl = gets.chomp.to_i
-print "Enter the height of the trapezium: "
-h = gets.chomp.to_i
-resultado = (bu + bl)*h/2
-print "The area of trapezium is: #{resultado}"
