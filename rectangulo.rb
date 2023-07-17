7257289bc61daada3cc02dd37f1d9cfec089623e
JuaanDuque <132398922+JuaanDuque@users.noreply.github.com>
2023-07-17 09:42:36 -0500

    Delete rectangulo.rb

--- rectangulo.rb
+++
@@ -1,7 +0,0 @@
-puts "Calculate area of a rectangle"
-print "Enter length of the rectangle: "
-length = gets.chomp.to_i
-print "Enter width of the rectangle: "
-width = gets.chomp.to_i
-resultado = length * width
-print "The area of the rectangle of width " +width.to_s+ " and length " +length.to_s+ " is: #{resultado}"
