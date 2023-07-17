166f2006d4f861418eaca52ec0d20b283bd02508
JuaanDuque <132398922+JuaanDuque@users.noreply.github.com>
2023-07-17 09:42:19 -0500

    Delete cuadrado.rb

--- cuadrado.rb
+++
@@ -1,5 +0,0 @@
-puts "Calculate area of a square"
-puts "Enter the value of the sides of the square: "
-L =  gets.chomp.to_i
-resultado = L*L
-print "The area of the square of sides: " +L.to_s+ " is: #{resultado}"
